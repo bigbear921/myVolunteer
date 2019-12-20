e0b57ce9859f66ff88d62506f811779ed98f9784
bigbear <18516104019@163.com>
2019-12-20 13:44:19 +0800

    no message

--- src/test/java/com/mfkj/myvolunteer/MyvolunteerApplicationTests.java
+++ src/test/java/com/mfkj/myvolunteer/MyvolunteerApplicationTests.java
@@ -1,9 +1,8 @@
 package com.mfkj.myvolunteer;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.Test;
 
-@SpringBootTest
+//@SpringBootTest
 class MyvolunteerApplicationTests {
 
 	@Test
